367499f098aa008767baa31d7b8b78b5d79bbec8
Илья Бояркин <iliya9401@live.com>
2023-07-27 12:33:59 +0600

    123

--- ManagerProfiles/MySQL.cs
+++ ManagerProfiles/MySQL.cs
@@ -76,7 +76,7 @@ namespace ManagerProfiles
         //private string port = "3306"; // Порт для подключения
         //private string password = ""; // Пароль для подключения
 
-        private string serverName = "astf3-stp5"; // Адрес сервера (для локальной базы пишите "localhost")
+        private string serverName = "192.168.0.10"; // Адрес сервера (для локальной базы пишите "localhost")
         private string userName = "root"; // Имя пользователя
         private string dbName = "clicker"; //Имя базы данных
         private string password = "Fralkon"; // Пароль для подключения
